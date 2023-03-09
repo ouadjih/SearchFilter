19ce6888f9e33140057c17d0ef2fdd96a1144760
ouadjih <47463757+ouadjih@users.noreply.github.com>
2023-03-09 12:30:35 +0100

    added filter project

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { Container } from "postcss";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">! Contact-info</h1>
+      <div className="container mx-auto px-4 bg:color">
+        <table class="">
+          <thead>
+            <tr>
+              <th>Song</th>
+              <th>Artist</th>
+              <th>Year</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+              <td>Malcolm Lockyer</td>
+              <td>1961</td>
+            </tr>
+            <tr>
+              <td>Witchy Woman</td>
+              <td>The Eagles</td>
+              <td>1972</td>
+            </tr>
+            <tr>
+              <td>Shining Star</td>
+              <td>Earth, Wind, and Fire</td>
+              <td>1975</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
